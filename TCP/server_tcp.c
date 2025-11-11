1f8b1dcfb91fb97d490f9e5d9bdc29b88e0f627f
MahekShah12 <mahekshah8910@gmail.com>
2025-11-11 17:50:13 +0000

    Added TCP and UDP practical programs

---
+++ TCP/server_tcp.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 10143
+#define BUFFER_SIZE 1024
+
+int main() {
+int server_fd, new_socket;
+struct sockaddr_in address;
+int addrlen = sizeof(address);
+char buffer[BUFFER_SIZE];
+FILE *fp;
+
+// Create socket
+server_fd = socket(AF_INET, SOCK_STREAM, 0);
+if (server_fd < 0) {
+perror("Socket creation failed");
+exit(1);
+}
+
+// Bind socket
+address.sin_family = AF_INET;
+address.sin_addr.s_addr = INADDR_ANY;
+address.sin_port = htons(PORT);
+
+if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+perror("Bind failed");
+exit(1);
+}
+
+// Listen
+if (listen(server_fd, 3) < 0) {
+perror("Listen failed");
+exit(1);
+}
+printf("Server listening on port %d...\n", PORT);
+
+// Accept connection
+new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+if (new_socket < 0) {
+perror("Accept failed");
+exit(1);
+}
+printf("Client connected!\n");
+
+// Receive file name first
+char filename[256];
+int name_len = recv(new_socket, filename, sizeof(filename), 0);
+if (name_len <= 0) {
+perror("Failed to receive filename");
+exit(1);
+}
+filename[name_len] = '\0';
+printf("Receiving file: %s\n", filename);
+
+// Open file for writing
+fp = fopen(filename, "wb");
+if (!fp) {
+perror("File open failed");
+exit(1);
+}
+
+// Receive and write file content
+ssize_t bytes;
+while ((bytes = recv(new_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+fwrite(buffer, 1, bytes, fp);
+buffer[bytes] = '\0';
+printf("%s", buffer); // Live print
+}
+
+printf("\n\nFile '%s' received successfully.\n", filename);
+
+fclose(fp);
+close(new_socket);
+close(server_fd);
+return 0;
+}
